e96dc451059f39fa925ed8aa9920dda9dec1d4a2
Ashok Xavier <ashokxavier2003@gmail.com>
2026-08-22 20:14:53 +0530

    Inheritance programs

--- work7.py
+++ work7.py
@@ -49,7 +49,7 @@ b=BankAccount("ashok",25000)
 b.deposit()
 b.withdrawal()
 b.balance_display()
-"""
+
 #3
 class Vehicle:
   def show(self):
@@ -67,3 +67,64 @@ c=Car()
 c.show()
 b=Bike()
 b.show()
+"""
+#4
+
+class Animal:
+  def __init__(self,name):
+    self.name=name
+    print(f"Animal name:{self.name}")
+
+  def eat(self):
+    print(f"{self.name} is eating")
+
+class Dog(Animal):
+  def __init__(self, name,breed):
+    Animal.__init__(self,name)
+    Animal.eat(self)
+    self.breed=breed
+    print(f"Dog breed: {self.breed}")
+
+  def bark(self):
+    print(f"{self.name} is barking")
+class Cat(Animal):
+  def __init__(self, name,color):
+    Animal.__init__(self,name)
+    Animal.eat(self)
+    self.color=color
+    print(f"Cat color: {self.color}")
+  def meow(self):
+    print(f"Cat name: {self.name}")
+
+d=Dog("Tomy","Lab")
+d.bark()
+c=Cat("kitty","white")
+c.meow()
+
+#5
+class GrandParent:
+  def __init__(self):
+    self.house="Big House"
+
+  def show_grandparent(self):
+    print(self.house)
+
+class Parent(GrandParent):
+  def __init__(self):
+    super().__init__()
+    self.car="BMW"
+  def show_car(self):
+    print(self.car)
+
+class Child(Parent):
+  def __init__(self):
+    super().__init__()
+    self.bike="Pulsar"
+
+  def show_bike(self):
+    print(self.bike)
+
+c=Child()
+c.show_bike()
+c.show_car()
+c.show_grandparent()
